b4d076bc815703ba0f45e2d97628ffbe3117d1e4
TheTrompas17 <abrahaminfor@gmail.com>
2022-11-10 15:25:46 +0100

    segundo commit

--- canviDbase/src/cosesmeues/Consola.java
+++ canviDbase/src/cosesmeues/Consola.java
@@ -8,13 +8,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- *
- * @author Joaquín
- */
+
+
 public class Consola {
+    /**
+* llig una cadena de text de teclat.
+* @return la cadena llegida
+* @throws IOException
+*/
+
 public static String lligText() throws IOException {
  BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
  return lector.readLine();
 }     
+
+/**
+* llig un enter de teclat. el procés de lectura es repeteix mentre el valor no siga un enter
+* @return el número llegit
+* @throws IOException
+*/
+
+public static int lligEnter() throws IOException {
+ BufferedReader lector = new BufferedReader(new InputStreamReader(System.in));
+ boolean incorrecte = true;
+ do {
+ try {
+ int valor = Integer.parseInt(lector.readLine()); 
+ return valor;
+  } catch (NumberFormatException ex) { // la transformació falla i s'agafa l'excepció
+ }
+ } while (incorrecte); // es repeteix mentre la transformació falla
+ return -1;
+}
+
 }
